09d46015b6326fbc50f462e71571f299b019befb
SANEH69 <sanehthandoh02@gmail.com>
2025-05-07 20:33:15 +0200

    Repositioned my background image

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function fetchRecipe(event) {
     event.preventDefault()
     let searchRecipeElement = document.querySelector("#search-recipe");
     let apiKey = "04d1784de2be03a1bd2o2db8tf6b23e4";
-    let context = "You are a very brilliant virtual assistant that know so much about cooking and recipes. Please make it a list with bullet points. Please make it prettier. Please leave line spaces between ingredients, the preparation and the serving.";
+    let context = "You are a very brilliant virtual assistant that know so much about cooking and recipes. Please do not make it a paragraph but a list with bullet points. Make sure the subtitles are in a bold font. Do not include the 'servings' section. Please skip the formalities and just get straight to the point.";
     let prompt = `Please provide a recipe about ${searchRecipeElement.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
